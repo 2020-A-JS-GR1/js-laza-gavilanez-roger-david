97c7f4b9f3f440b8bd367f7568050d562bea5bad
Laza Roger <roger.laza@epn.edu.ec>
2020-06-12 17:19:30 -0500

    Variables y typeof

---
+++ 01-javascript/01-variables.js
@@ -0,0 +1,24 @@
+//Mutables e Inmutables
+//Mutables
+var numeroUno=1;
+let numeroDos=2;
+numeroUno=5;
+numeroDos=8;
+numeroUno=false;
+numeroDos=true;
+//Inmutables
+const configuracionArchivo="PDF";
+//configuracionArchivo="XML";
+const numero=1;//number
+const sueldo="1.1";
+const texto="Adrian";//string
+const booleanooo="false";//boolean
+const hijos=null;//object
+const zapatos=undefined;//undefine
+console.log(typeof numero);
+console.log(typeof sueldo);
+console.log(typeof texto);
+console.log(typeof booleanooo);
+console.log(typeof hijos);
+console.log(typeof zapatos);
+
