ad37ea2934c5c32edf6a3b55bc4eef5b6062d158
Laza Roger <roger.laza@epn.edu.ec>
2020-06-30 18:29:13 -0500

    Callbacks y Promesas

--- 01-javascript/06-callbacks.js
+++ 01-javascript/06-callbacks.js
@@ -1,16 +1,27 @@
 const fs = require('fs');
 console.log('Primero');
 
-fs.readFile(
-   './06-ejemplo.txt',
-   'utf-8',
+// fs.readFile(// callback
+//    './06-ejemplo.txt',
+//    'utf-8',
+//     (error,contenido)=>{
+//        if(error){
+//            console.error('Hubo error',error);
+//        }else{
+//            console.log(contenido);
+//        }
+//     }
+// );
+
+fs.readFile(// callback
+    './06-ejemplo.txt',
+    'utf-8',
     (error,contenido)=>{
-       if(error){
-           console.error('Hubo error',error);
-       }else{
-           console.log(contenido);
-       }
+        if(error){
+            console.error('Hubo error',error);
+        }else{
+            console.log(contenido);
+        }
     }
 );
-
 console.log('Final');
