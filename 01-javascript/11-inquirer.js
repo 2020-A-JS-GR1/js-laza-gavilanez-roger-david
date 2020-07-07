1fe76867c327c9bf6c397bafdfca1f20a28588aa
Laza Roger <roger.laza@epn.edu.ec>
2020-07-07 18:07:40 -0500

    JS

--- 01-javascript/11-inquirer.js
+++ 01-javascript/11-inquirer.js
@@ -1,18 +1,26 @@
 const inquirer = require('inquirer');
 async function main(){
-    const respuesta = await inquirer.prompt([
-        {
-            type: 'input',
-            name: 'apellido',
-            message: 'Ingresa Tu Nombre'
-        },
-        {
-            type: 'input',
-            name: 'edad',
-            message: 'Ingresa tu edad'
+    try {
+        const respuesta = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'apellido',
+                message: 'Ingresa Tu Nombre'
+            },
+            {
+                type: 'input',
+                name: 'edad',
+                message: 'Ingresa tu edad'
 
-        }
-    ]);
-    console.log('Respuesta',respuesta);
+            }
+        ]);
+        console.log('Respuesta',respuesta);
+
+    }catch (e) {
+        console.log(e);
+    }
+
 }
 main();
+//CRUD --> CON ESTO ENTIDADES5 DATOS TRING INT BOOL
+//UNO A MUCHOS GUUARDADOS
